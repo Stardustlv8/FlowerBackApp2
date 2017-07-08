6aa023fd99139ed199c1f24b1accb2abc52a0bbe
Aviel Aldama <aviel.aldama@gmail.com>
2017-07-07 19:50:59 -0500

    Add project files.

---
+++ FlowerBackApp2/FlowerBackApp2/Infrastructure/InstanceLocator.cs
@@ -0,0 +1,13 @@
+﻿using FlowerBackApp2.ViewModels;
+
+namespace FlowerBackApp2.Infrastructure
+{
+    class InstanceLocator
+    {
+        public MainViewModel Main { get; set; }
+        public InstanceLocator()
+        {
+            Main = new MainViewModel();
+        }
+    }
+}
---
+++ FlowerBackApp2/FlowerBackApp2/Pages/FlowersPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FlowerBackApp2.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class FlowersPage : ContentPage
+    {
+        public FlowersPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
